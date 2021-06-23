2ba6e8a1e94624e2fcacb54a998c2cbc39869ada
shihaipeng03 <wangjd76@gmail.com>
2021-06-23 11:18:26 +0800

    2106

--- BOX版程序/2 有时钟模块版/plotclock/plotclock.ino
+++ BOX版程序/2 有时钟模块版/plotclock/plotclock.ino
@@ -1,8 +1,8 @@
 //淘宝『有名称的店铺』https://somebodys.taobao.com/
-//更新日期 2021/06/04
+//更新日期 2019/07/13
 
-//视频教程：小贱钟的安装程序 https://www.bilibili.com/video/BV1Et411J7vD/
-//最新制作了web版编译器的使用教程视频，还有更多视频内容正在上传请关注我们的B站 https://space.bilibili.com/30602531
+//视频教程：小贱钟的安装程序 https://www.ixigua.com/i6713939566358692356/
+//最新制作了web版编译器的使用教程视频，还有更多视频内容正在上传请关注我们的西瓜视频 https://www.ixigua.com/i6713192503262052871/
 
 //https://create.arduino.cc/editor/wjd76/30f046de-337f-45ae-ba1e-f39a50179be3/preview 
 //最新增加了一个可以写字母的程序，高手可以研究。
@@ -10,29 +10,30 @@
 //如果需要运行调试摆臂的程序，打开此代码，如果需要运行主程序，请注释掉下一行代码↓↓↓↓↓↓↓
 //#define CALIBRATION 
 
-//↓↓↓↓↓↓↓  以下参数在调试程序中调节好后复制过来   ↓↓↓↓↓↓↓
+
+//↓↓↓↓↓↓↓  以下参数需要配合舵机摆臂运行位置进行修正，否则写字可能是歪的，甚至无法正常写字   ↓↓↓↓↓↓↓
 
 
 //1.先调节0、180度的位置。调节到位后，再调节90度位置
 //左右悬臂舵机的 0或180度位置,，数字增加，左侧舵机逆时针调整，右侧舵机顺时针调整
 //【此数值可能需要调节】
-#define SERVOLEFTNULL 2030  //数值减小，顺时针旋转，加大则逆时针旋转
+#define SERVOLEFTNULL 1930  //数值减小，顺时针旋转，加大则逆时针旋转 默认2000
 //【此数值可能需要调节】
-#define SERVORIGHTNULL 1010  //数值减小，顺时针旋转，加大则逆时针旋转
+#define SERVORIGHTNULL 960  //数值减小，顺时针旋转，加大则逆时针旋转  默认1000
 
 //2.调节到位0、180，再调节下面参数
 //左右悬臂舵机的90度位置,，数字增加，左侧舵机顺时针调整，右侧舵机逆时针调整
 //【此数值可能需要调节】
-#define SERVOFAKTORLEFT 600 //数值加大，顺时针旋转，减小则逆时针旋转
+#define SERVOFAKTORLEFT 690 //数值加大，顺时针旋转，减小则逆时针旋转 默认650
 //【此数值可能需要调节】
-#define SERVOFAKTORRIGHT 605  //数值减小，顺时针旋转，加大则逆时针旋转
+#define SERVOFAKTORRIGHT 620  //数值减小，顺时针旋转，加大则逆时针旋转 默认650
 
 
 //升举舵机的3个角度 
 //【此数值可能需要调节】
-#define LIFT0 2000 //落笔写字 on drawing surface
-#define LIFT1 2100  //写字时抬臂动作 between numbers
-#define LIFT2 2200  //高抬笔架  going towards sweeper 
+#define LIFT0 1830 //落笔写字 
+#define LIFT1 1730  //写字时抬臂动作 一般比lift0 小100左右
+#define LIFT2 1600  //高抬笔架  
 //【此数值可能需要调节】
 
 
@@ -40,8 +41,8 @@
 //以上参数，请运行调试程序 plotclockadj ，调整好位置后，将数据复制过来
 //
 
-//笔擦的坐标位置，如不能对准笔擦可以微调单位毫米
-int  rubberx=72,rubbery=42;   //【此数值可能需要调节】
+//笔擦的坐标位置，摆臂调节好后，如不能对准笔擦可以微调此参数，单位毫米
+int  rubberx=82,rubbery=48;   //【此数值可能需要调节】
 
 //三只舵机的接口号
 #define SERVOPINLIFT  2 //抬臂舵机
@@ -53,18 +54,26 @@ int  rubberx=72,rubbery=42;   //【此数值可能需要调节】
 
 // 悬臂的长度，根据图纸测量，无需改变
 #define L1 35
-#define L2 57.2
-#define L3 14.2
+#define L2 57.1
+#define L3 14
 
 //左右舵机轴心的位置，根据图纸测量，无需改变
-#define O1X 22 //box版 28.7，普通版22
+#define O1X 28.7 //box版 28.7，普通版22
 #define O1Y -25
-#define O2X 47 //box版53.9， 普通版47
+#define O2X 53.9 //box版53.9， 普通版47
 #define O2Y -25
 
-
-
+//需要的库函数
 #include <Servo.h>
+#include <DS3231.h>
+#include <Wire.h>
+
+//时钟模块，如果没有时钟模块，请用另一个程序
+DS3231 Clock;  
+bool h12;
+bool PM;
+
+
 
 int servoLift = LIFT2;
 
@@ -76,100 +85,100 @@ volatile double lastX = rubberx;
 volatile double lastY = rubbery;
 
 int last_min = 0;
-long reference_hour,reference_minutes,reference_sec; //起始的基准时间
 
 void setup() 
 { 
-  //设置起始时间，（小时，分钟，秒）
-  settime(12,34,0);  //每次通电后从12:34 开始计时
+  Wire.begin();
+  //第一次运行 请设置时间，设置成功后请删除此行代码,或者如下格式注释掉
+  //Clock.setMinute(06);//设置分钟 
+  //Clock.setHour(14);  //设置小时 
 
+  //第一次运行 请设置时间，设置成功后请删除此行代码
+  
   servo1.attach(SERVOPINLIFT);  //初始化抬臂舵机  lifting servo
   servo2.attach(SERVOPINLEFT);  //初始化左臂舵机  left servo
   servo3.attach(SERVOPINRIGHT); //初始化右臂舵机  right servo
   lift(2); //抬笔
   delay(300);
+  drawTo(rubberx/2, rubbery/2);
+  delay(500);
   drawTo(rubberx, rubbery); //停留在笔擦位置
-  delay(1000);
+  delay(800);
 
 } 
 
 void loop() 
 { 
-
+int minute,hour; 
+minute=Clock.getMinute();
+hour=Clock.getHour(h12, PM);//读取时间
+  
 int i = 0;
+#ifdef CALIBRATION  //条件编译，当定义了CALIBRATION 则编译下面99~117行 调节摆臂的代码，否则编译更下面正常运行的代码。
+ 
+  //舵机摆在90°的区域内往复运动，调节舵机参数让摆臂平行于x轴和y轴
+   
+  drawTo(6.1, 30.7); //box版
+  delay(1000);
+   
+  drawTo(83.3, 26.7); //让笔尖运行到此位置，正常情况两摆臂将垂直或平行于xy轴，根据运行情况，调节舵机参数让摆臂接近垂直或水平状态，详细操作见说明文档
+  delay(1000);     //*/
+
+ /* //如需调节抬臂高度，可以去掉注释，编译运行下面代码。
+  //每个角度停留1秒钟，观察角度位置，调整LIFT0~2 三个参数
+  lift(2);//高抬笔架
+  delay(1000);
+  lift(1);//写字时抬臂动作
+  delay(1000);
+  lift(0);//落笔写字 让摆臂龙门架刚好垂直，此位置最佳
+  delay(1000);
+  //*/
+
+#else 
+
   //下面代码任选一行，进行不同模式的书写
   //if (last_min != minute())  //每分钟书写一次时间
   //if  (1)                  //反复不间断的擦写模式
   
-  if (1) //如需修改请复制上面代码
+  if (1)  //如需修改请复制上面代码
   {
+
     if (!servo1.attached()) servo1.attach(SERVOPINLIFT);
     if (!servo2.attached()) servo2.attach(SERVOPINLEFT);
     if (!servo3.attached()) servo3.attach(SERVOPINRIGHT);
 
     lift(0);
 
-    get_hour();
-    while ((i+1)*10 <= get_hour())
+    
+    while ((i+1)*10 <= hour)
     {
       i++;
     }
 
     number(3, 3, 111, 1);
     number(5, 25, i, 0.9);
-    number(19, 25, (get_hour()-i*10), 0.9);
+    number(19, 25, (hour-i*10), 0.9);
     number(28, 25, 11, 0.9);
 
     i=0;
-    while ((i+1)*10 <= get_minute())
+    while ((i+1)*10 <= minute)
     {
       i++;
     }
     number(34, 25, i, 0.9);
-    number(48, 25, (get_minute()-i*10), 0.9);
+    number(48, 25, (minute-i*10), 0.9);
     lift(2);
-    drawTo(70, 44);
+    drawTo(rubberx, rubbery);
     lift(1);
-    last_min = get_minute();
+    last_min = minute;
 
     servo1.detach();
     servo2.detach();
     servo3.detach();
   }
 
-
+#endif
 } 
-
-//设置起始时间（小时，分钟，秒）
-void settime(int ih,int im,int is) {
-  reference_hour=ih % 24; //求余数 防止溢出
-  reference_minutes=im % 60;
-  reference_sec = is % 60;
-}
-
-//获得修正后的小时数
- long get_hour(){
-   long sec = get_seconds();
-   long ho = sec / 3600;
-  ho = ho % 24;  //24或12小时制
-  return(ho);
-}
-
-//获得修正后的分钟数
- long get_minute(){
-    long sec = get_seconds();
-    long mi = sec / 60 ;
-    mi = mi % 60;
- return( mi) ;
-}
-
-//获得Arduino的启动时长 + 修正时间
- long  get_seconds(){
-   long se = millis()/1000; 
-  se = se +reference_sec + reference_minutes*60 + reference_hour*3600; 
- return(se);
-}
-
 
 // Writing numeral with bx by being the bottom left originpoint. Scale 1 equals a 20 mm high font.
 // The structure follows this principle: move to first startpoint of the numeral, lift down, draw numeral, lift up
@@ -279,7 +288,7 @@ void number(float bx, float by, int num, float scale) {
     drawTo(2, 20);
     drawTo(60, rubbery);
 
-    drawTo(rubberx + 3 , rubbery);
+    drawTo(rubberx + 1 , rubbery);
     lift(2);
 
     break;
@@ -299,12 +308,12 @@ void number(float bx, float by, int num, float scale) {
 }
 
 
-//抬函数，不同的摆臂高度
+//抬臂函数，不同的摆臂高度
 void lift(char lift) {
   switch (lift) {
     // room to optimize  !
 
-  case 0: 
+  case 0: //850
 
       if (servoLift >= LIFT0) {
       while (servoLift >= LIFT0) 
@@ -326,7 +335,7 @@ void lift(char lift) {
 
     break;
 
-  case 1: 
+  case 1: //150
 
     if (servoLift >= LIFT1) {
       while (servoLift >= LIFT1) {
@@ -444,8 +453,8 @@ void set_XY(double Tx, double Ty)
 
   // calculate joinr arm point for triangle of the right servo arm
   a2 = return_angle(L2, L1, c);
-  Hx = Tx + L3 * cos((a1 - a2 + 0.621) + M_PI); //36,5掳
-  Hy = Ty + L3 * sin((a1 - a2 + 0.621) + M_PI);
+  Hx = Tx + L3 * cos((a1 - a2 + 0.45937) + M_PI); //36,5掳  更正 原版是 35.5°，不是 36,5掳，新版修改后是26.32°
+  Hy = Ty + L3 * sin((a1 - a2 + 0.45937) + M_PI); //感谢武汉网友 自由华莱士 的鼎力帮助，特此鸣谢
 
   // calculate triangle between pen joint, servoRight and arm joint
   dx = Hx - O2X;
